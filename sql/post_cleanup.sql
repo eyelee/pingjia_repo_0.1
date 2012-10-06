d63ab6a94d3c5270a42395f9fc581152d8e15de9
Kevin Wan <kevin.wanch@gmail.com>
2012-10-06 11:08:09 -0700

    update cleanup.sql

--- sql/post_cleanup.sql
+++ sql/post_cleanup.sql
@@ -1,1 +1,23 @@
-update open_product set time= subdate(time,INTERVAL 1 YEAR) WHERE time > curdate();
+/*
+select count(*) from open_product as by_product
+union
+select count(*) from open_by_model as  by_model
+union
+select count(*) from open_by_year as by_year;
+*/
+delete from  open_product WHERE time > current_timestamp();
+delete from  open_product WHERE price > 200 or price is Null;
+call init_by_model;
+call init_by_year;
+call cleanup_before_sync;
+
+/*
+select count(*) from open_product as by_product
+union
+select count(*) from open_by_model as  by_model
+union
+select count(*) from open_by_year as by_year;
+*/
+
+call sync_by_model;
+call sync_by_year;
